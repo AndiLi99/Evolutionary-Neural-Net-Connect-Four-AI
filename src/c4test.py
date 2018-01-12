671127bd0cff5536fc2885110fac3d96cc2f1435
a259l <a259li@uwaterloo.ca>
2018-01-12 03:48:06 -0500

    genetic modification

--- src/c4test.py
+++ src/c4test.py
@@ -9,60 +9,61 @@ import random
 
 pop0 = population.load_population("pop_gen0.txt")
 pop100 = population.load_population("pop_gen100.txt")
-# net100 = pop100.population[0]
+net100 = pop100.population[0]
 
-w = 0
-t = 0
-l = 0
-for k in range(20):
-    board = np.zeros((6,7))
-    net100 = pop100.population[random.randint(0,19)]
-    net0 = pop0.population[random.randint(0,19)]
-    win = fitness.compete(net40, net0)
-    print ("WINNER:" + str(win))
-    if win == 1:
-        w+=1
-    elif win == 0:
-        t+=1
-    else:
-        l+=1
-    move = input("make a move: ")
-
-
-print("WINS: %d  TIES: %d  LOSS: %d"%(w,t,l))
-
-w = 0
-t = 0
-l = 0
-for k in range(20):
-    board = np.zeros((6,7))
-    net100 = pop100.population[random.randint(0,19)]
-    net0 = pop0.population[random.randint(0,19)]
-    win = fitness.compete(net0, net40)
-    print ("WINNER:" + str(win))
-    if win == 1:
-        w+=1
-    elif win == 0:
-        t+=1
-    else:
-        l+=1
-    move = input("make a move: ")
-
-print("WINS: %d  TIES: %d  LOSS: %d"%(w,t,l))
-#
-# while True:
+# w = 0
+# t = 0
+# l = 0
+# for k in range(20):
 #     board = np.zeros((6,7))
-#     while(connectFour.checkWinner(board) ==2):
-#         move = input("make a move: ")
-#         if not connectFour.check_valid(board, move):
-#             continue
-#         #connectFour.play(board, 1, minimax.pickMove(board, 1, 3, net0))
-#         connectFour.play(board, 1, move)
-#         connectFour.print_board(board)
-#         # raw_input("press")
-#         print
-#         if not connectFour.checkWinner(board)==2:
-#             break
-#         connectFour.play(board, -1, minimax.pickMove(board, -1, 3, net100))
-#         connectFour.print_board(board)
-#     print ("WINNER:" + str(connectFour.checkWinner(board)))
+#     net100 = pop100.population[random.randint(0,19)]
+#     net0 = pop0.population[random.randint(0,19)]
+#     win = fitness.compete(net40, net0)
+#     print ("WINNER:" + str(win))
+#     if win == 1:
+#         w+=1
+#     elif win == 0:
+#         t+=1
+#     else:
+#         l+=1
+#     move = input("make a move: ")
+#
+#
+# print("WINS: %d  TIES: %d  LOSS: %d"%(w,t,l))
+#
+# w = 0
+# t = 0
+# l = 0
+# for k in range(20):
+#     board = np.zeros((6,7))
+#     net100 = pop100.population[random.randint(0,19)]
+#     net0 = pop0.population[random.randint(0,19)]
+#     win = fitness.compete(net0, net40)
+#     print ("WINNER:" + str(win))
+#     if win == 1:
+#         w+=1
+#     elif win == 0:
+#         t+=1
+#     else:
+#         l+=1
+#     move = input("make a move: ")
+#
+# print("WINS: %d  TIES: %d  LOSS: %d"%(w,t,l))
+
+while True:
+    board = np.zeros((6,7))
+    connectFour.print_board(board)
+    while(connectFour.checkWinner(board) ==2):
+        move = input("make a move: ")
+        if not connectFour.check_valid(board, move):
+            continue
+        #connectFour.play(board, 1, minimax.pickMove(board, 1, 3, net0))
+        connectFour.play(board, 1, move)
+        connectFour.print_board(board)
+        # raw_input("press")
+        print
+        if not connectFour.checkWinner(board)==2:
+            break
+        connectFour.play(board, -1, minimax.pickMove(board, -1, 3, net100))
+        connectFour.print_board(board)
+    print ("WINNER:" + str(connectFour.checkWinner(board)))
--- src/geneticTest.py
+++ src/geneticTest.py
@@ -7,11 +7,11 @@ import individual
 import random
 
 pop = population.Population(layer_types=["conv", "dense", "soft"], layer_shapes=[[(1, 6, 7), (4, 1, 4, 4)], [(20, 4*3*4)], [(2, 20)]], initial_pop=20)
-pop = population.load_population("pop_gen100.txt")
+# pop = population.load_population("pop_gen100.txt")
 # net = pop.population[0]
 # net.save("net100.txt")
 
-for x in range(101, 100000000):
+for x in range(0, 100000000):
     pop.evolve(12)
     print("gen " + str(x))
     if x%2 == 0:
