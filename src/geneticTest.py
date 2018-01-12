d58d92a5411daa4821f27e4f70ecc2f0835bdea5
maxgao11111 <maxgao18@yahoo.com>
2018-01-12 03:46:24 -0500

    Training population

--- src/geneticTest.py
+++ src/geneticTest.py
@@ -6,14 +6,21 @@ from minimax import pickMove
 import individual
 import random
 
-pop = population.Population(layer_types=["conv", "dense", "soft"], layer_shapes=[[(1, 6, 7), (4, 1, 4, 4)], [(20, 4*3*4)], [(2, 20)]], initial_pop=20)
-pop = population.load_population("pop_gen100.txt")
+pop = population.Population(initial_pop=20, layer_types=["conv", "dense", "soft"],
+                            layer_shapes=[[(1, 6, 7), (4, 1, 4, 4)], [(20, 4*3*4)], [(2, 20)]],
+                            conv_layer_types=[("full",2)])
+# pop.save("test1.txt")
+# print(pop.population[10].get_layers()[0].get_filter(2).get_weights())
+# p2 = population.load_population("test1.txt")
+#
+# p2.save("test2.txt")
+# print(p2.population[10].get_layers()[0].get_filter(2).get_weights())
 # net = pop.population[0]
 # net.save("net100.txt")
 
-for x in range(101, 100000000):
+for x in range(0, 100000000):
     pop.evolve(12)
     print("gen " + str(x))
-    if x%2 == 0:
-        pop.save("pop_gen"+str(x)+".txt")
-    #pop.save("pop_gen" + str(x) + ".txt")
+    # if x%2 == 0:
+    #     pop.save("pop_gen"+str(x)+".txt")
+    pop.save("pop_gen" + str(x) + ".txt")
